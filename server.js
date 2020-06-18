95693948a018fcc6ef03cc106733c0e381864c24
Peter Chan <65880423+peterkh2@users.noreply.github.com>
2020-06-18 10:18:46 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var pg = require('pg');
+var fs = require('fs');
 var app = express();
 
 app.set('port', process.env.PORT || 5000);
