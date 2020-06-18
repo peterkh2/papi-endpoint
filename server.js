49b9f8471236e50ee6148f9a63ce8ccdadb3814c
Peter Chan <65880423+peterkh2@users.noreply.github.com>
2020-06-18 10:45:22 +0800

    Update server.js

--- server.js
+++ server.js
@@ -9,43 +9,43 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 // Original API
-// // app.get('/api-endpoint', function(request, response) {
-//     var nameString = request.query.name;
-//     var historyString = request.query.history;
+app.get('/api-endpoint', function(request, response) {
+    var nameString = request.query.name;
+    var historyString = request.query.history;
     
-//       var jsonContent = 
-//           [
-//                         {
-//                             "id": 1,
-//                             "productImage": "https://www.hkcsl.com/u/cms/pccw/img/handset/20111_2.jpg",
-//                             "productLink": "https://www.hkcsl.com/handset/tc/pro_details.jsp?id=421070",
-//                             "status": "Success",
-//                             "statusStyle": "slds-text-color_success",
-//                             "productName": "Black Shark 2 Pro (12GB+256GB)",
-//                             "purchaseDate": "05-May-2019",
-//                             "paymentMethod": "Cash",
-//                             "purchasePrice": "4898",
-//                             "purchaseMethod": "Walk-in"
-//                         }
-//       ,
-//                         {
-//                             "id": 2,
-//                             "productImage": "https://www.hkcsl.com/u/cms/pccw/img/handset/38221_2.jpg",
-//                             "productLink": "https://www.hkcsl.com/handset/tc/pro_details.jsp?id=421070",
-//                             "status": "Cancel",
-//                             "statusStyle": "slds-text-color_destructive",
-//                             "productName": "iPhone SE (128GB)",
-//                             "purchaseDate": "05-Jun-2020",
-//                             "paymentMethod": "Credit card",
-//                             "purchasePrice": "3899",
-//                             "purchaseMethod": "Website"
-//                         }        
-//                 ]
-//       ;
-//     response.setHeader('Access-Control-Allow-Origin','*');
-//     response.send(JSON.parse(JSON.stringify(jsonContent)));
+      var jsonContent = 
+          [
+                        {
+                            "id": 1,
+                            "productImage": "https://www.hkcsl.com/u/cms/pccw/img/handset/20111_2.jpg",
+                            "productLink": "https://www.hkcsl.com/handset/tc/pro_details.jsp?id=421070",
+                            "status": "Success",
+                            "statusStyle": "slds-text-color_success",
+                            "productName": "Black Shark 2 Pro (12GB+256GB)",
+                            "purchaseDate": "05-May-2019",
+                            "paymentMethod": "Cash",
+                            "purchasePrice": "4898",
+                            "purchaseMethod": "Walk-in"
+                        }
+      ,
+                        {
+                            "id": 2,
+                            "productImage": "https://www.hkcsl.com/u/cms/pccw/img/handset/38221_2.jpg",
+                            "productLink": "https://www.hkcsl.com/handset/tc/pro_details.jsp?id=421070",
+                            "status": "Cancel",
+                            "statusStyle": "slds-text-color_destructive",
+                            "productName": "iPhone SE (128GB)",
+                            "purchaseDate": "05-Jun-2020",
+                            "paymentMethod": "Credit card",
+                            "purchasePrice": "3899",
+                            "purchaseMethod": "Website"
+                        }        
+                ]
+      ;
+    response.setHeader('Access-Control-Allow-Origin','*');
+    response.send(JSON.parse(JSON.stringify(jsonContent)));
     
-// });
+});
 
 //Purchase History
 app.get('/api/purchaseHistory', function (request, response) {
